55659d4264b79258b2399735f050c99926d101c1
storezhang <storezhang@gmail.com>
2023-01-12 13:36:38 +0800

    feat(发布): 支持附件上传

--- asset.go
+++ asset.go
@@ -3,15 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
+	"github.com/goexl/gox"
 	"github.com/goexl/gox/field"
 	"github.com/goexl/structer"
 )
 
 type asset struct {
-	File string `json:"file"`
-	Name string `json:"name"`
-	Label    string `json:"label"`
+	File  string `json:"file"`
+	Name  string `json:"name"`
+	Label string `json:"label"`
 }
 
 func (a *asset) upload(plugin *plugin, id int64) (err error) {
@@ -19,6 +21,17 @@ func (a *asset) upload(plugin *plugin, id int64) (err error) {
 	req := new(assetUploadReq)
 	ctx := context.Background()
 	uri := fmt.Sprintf("repos/%s/%s/releases/%d/assets", plugin.Owner, plugin.Repo, id)
+
+	if info, se := os.Stat(a.File); nil != se {
+		err = se
+		plugin.Info("打开文件出错", field.New("file", a.File), field.Error(se))
+	} else {
+		plugin.Info("准备上传文件", field.New("file", a.File), field.New("size", gox.Size(info.Size())))
+	}
+	if nil!=err{
+		return
+	}
+
 	if ce := structer.New().Map().From(a).To(req).Convert(); nil != ce {
 		err = ce
 		plugin.Warn("复制结构体出错", field.New("from", a), field.Error(ce))
