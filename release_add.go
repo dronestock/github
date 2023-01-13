075981d7bb7d7608ef12c9ad0cf7c6211734c2dc
storezhang <storezhang@gmail.com>
2023-01-13 15:46:07 +0800

    feat(发布): 描述信息支持从文件中读取

--- release_add.go
+++ release_add.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/goexl/gox"
 	"github.com/goexl/gox/field"
@@ -10,6 +11,11 @@ import (
 )
 
 func (r *release) add(plugin *plugin) (err error) {
+	// 优先假定配置是文件，读取文件中的内容充当描述信息
+	if bytes, re := os.ReadFile(r.Body); nil == re {
+		r.Body = string(bytes)
+	}
+
 	req := new(githubReleaseCreateReq)
 	rsp := new(githubRelease)
 	uri := fmt.Sprintf("repos/%s/%s/releases", plugin.Owner, plugin.Repo)
