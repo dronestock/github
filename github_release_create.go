dfdf212cbdbaa48dd6544c5f7f12bb0b8868fc7a
storezhang <storezhang@gmail.com>
2023-01-13 15:53:00 +0800

    feat(项目): 默认从Drone中取出仓库

--- github_release_create.go
+++ github_release_create.go
@@ -3,10 +3,10 @@ package main
 type githubReleaseCreateReq struct {
 	TagName                string `json:"tag_name,omitempty" structer:"tag"`
 	TargetCommitish        string `json:"target_commitish,omitempty"  structer:"target"`
-	Name                   string `json:"name,omitempty"  structer:"name"`
-	Body                   string `json:"body,omitempty"  structer:"body"`
-	Draft                  bool   `json:"draft,omitempty"  structer:"draft"`
-	Prerelease             bool   `json:"prerelease,omitempty"  structer:"prerelease"`
+	Name                   string `json:"name,omitempty" structer:"name"`
+	Body                   string `json:"body,omitempty" structer:"body"`
+	Draft                  bool   `json:"draft,omitempty" structer:"draft"`
+	Prerelease             bool   `json:"prerelease,omitempty" structer:"prerelease"`
 	DiscussionCategoryName string `json:"discussion_category_name,omitempty" structer:"discussion"`
 	GenerateReleaseNotes   bool   `json:"generate_release_notes,omitempty" structer:"notes"`
 	MakeLatest             string `json:"make_latest,omitempty" structer:"latest" validate:"oneof=true false legacy"`
--- release.go
+++ release.go
@@ -2,23 +2,23 @@ package main
 
 type release struct {
 	// 标签
-	Tag string `json:"tag" validate:"required"`
+	Tag string `json:"tag" validate:"required" structer:"tag"`
 	// 目标
-	Target string `json:"target"`
+	Target string `json:"target" structer:"target"`
 	// 名称
-	Name string `json:"name"`
+	Name string `json:"name" structer:"name"`
 	// 描述
-	Body string `json:"body"`
+	Body string `json:"body" structer:"body"`
 	// 是否是草稿
-	Draft bool `json:"draft"`
+	Draft bool `json:"draft" structer:"draft"`
 	// 是否是预发布
-	Prerelease bool `json:"prerelease"`
+	Prerelease bool `json:"prerelease" structer:"prerelease"`
 	// 如果指定，将创建指定类别的讨论并将其链接到发布
-	Discussion string `json:"discussion"`
+	Discussion string `json:"discussion" structer:"discussion"`
 	// 是否为本次发布自动生成名称和正文
-	Notes bool `json:"notes"`
+	Notes bool `json:"notes" structer:"notes"`
 	// 指定是否应将此版本设置为存储库的最新版本
-	Latest string `default:"true" json:"latest"`
+	Latest string `default:"true" json:"latest" structer:"latest"`
 
 	// 附件
 	Asset *asset `json:"asset"`
